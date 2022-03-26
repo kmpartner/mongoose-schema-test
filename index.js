d10a807512fb63f6c44307551e50d540ba0a69ac
Kaoru Masuyama <kmpartner@outlook.com>
2022-03-26 13:04:46 +0900

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-const TestSchema = require('./models/test');
+// const TestSchema = require('./models/test');
 
-const indexTest = 'index-test';
-console.log('TestSchema', TestSchema, typeof(TestSchema));
-console.log('indexTest', indexTest);
+// const indexTest = 'index-test';
+// console.log('TestSchema', TestSchema, typeof(TestSchema));
+// console.log('indexTest', indexTest);
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const testSchema = new Schema({
+    name: { type: String },
+    age: { type: Number},
+},
+{ timestamps: true }
+)
+
+module.exports = mongoose.model('Test', testSchema);
